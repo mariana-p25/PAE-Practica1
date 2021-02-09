264646f9c2a5cd9668f2329187cb44e8f930277a
mariana-p25 <mariana.pardo.a@gmail.com>
2021-02-09 15:54:28 -0600

    handlebars y grid view

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+declare var Handlebars: any;
+
+var url = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=df468393b9de44d490f48eebd19ff40f';
+var req = new Request(url);
+
+const news = fetch(req)
+    .then((res) => res.json())
+    .then((json) => {
+        const templateSource = document.getElementById('news-container').innerHTML;
+        const template = Handlebars.compile(templateSource);
+        document.getElementById('news-container').innerHTML = template({
+            news: json.articles
+        });
+    });
+
