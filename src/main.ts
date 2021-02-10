60a60d0783fb226b592aa9a796354ead6e7848f8
mariana-p25 <mariana.pardo.a@gmail.com>
2021-02-09 19:32:36 -0600

    grid funcionando

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 declare var Handlebars: any;
 
-var url = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=df468393b9de44d490f48eebd19ff40f';
+var url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=df468393b9de44d490f48eebd19ff40f';
+
 var req = new Request(url);
 
 const news = fetch(req)
     .then((res) => res.json())
     .then((json) => {
-        const templateSource = document.getElementById('news-container').innerHTML;
+        const templateSource = document.getElementById('news').innerHTML;
         const template = Handlebars.compile(templateSource);
-        document.getElementById('news-container').innerHTML = template({
+        document.getElementById('news').innerHTML = template({
             news: json.articles
         });
     });
 
+//var keyword;
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('keyword').addEventListener('change', () => {
+        console.log("!!!!!11");
+    });
+});
