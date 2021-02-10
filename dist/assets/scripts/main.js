b9c5e0019d0ffa298f5a63b224bece3679af3585
mariana-p25 <mariana.pardo.a@gmail.com>
2021-02-09 22:50:28 -0600

    searchbar

--- dist/assets/scripts/main.js
+++ dist/assets/scripts/main.js
@@ -1,17 +1,21 @@
-var url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=df468393b9de44d490f48eebd19ff40f';
-var req = new Request(url);
-const news = fetch(req)
-    .then((res) => res.json())
-    .then((json) => {
-    const templateSource = document.getElementById('news').innerHTML;
-    const template = Handlebars.compile(templateSource);
-    document.getElementById('news').innerHTML = template({
-        news: json.articles
+function loadNews(keyword) {
+    var url = 'https://newsapi.org/v2/everything?q=' + keyword + '&apiKey=df468393b9de44d490f48eebd19ff40f';
+    console.log(url);
+    var req = new Request(url);
+    fetch(req)
+        .then((res) => res.json())
+        .then((json) => {
+        const templateSource = document.getElementById('news').innerHTML;
+        const template = Handlebars.compile(templateSource);
+        document.getElementById('news').innerHTML = template({
+            news: json.articles
+        });
     });
-});
-//var keyword;
+}
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('keyword').addEventListener('change', () => {
-        console.log("!!!!!11");
+    loadNews('bit');
+    document.getElementById('buscar').addEventListener('click', () => {
+        let key = document.getElementById("keyword");
+        loadNews(key.value);
     });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
 declare var Handlebars: any;
 
-var url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=df468393b9de44d490f48eebd19ff40f';
+function loadNews(keyword) {
+    var url = 'https://newsapi.org/v2/everything?q=' + keyword + '&apiKey=df468393b9de44d490f48eebd19ff40f';
+    console.log(url);
+    var req = new Request(url);
 
-var req = new Request(url);
+    fetch(req)
+        .then((res) => res.json())
+        .then((json) => {
+            const templateSource = document.getElementById('news').innerHTML;
+            const template = Handlebars.compile(templateSource);
+            document.getElementById('news').innerHTML = template({
+                news: json.articles
+            });
 
-const news = fetch(req)
-    .then((res) => res.json())
-    .then((json) => {
-        const templateSource = document.getElementById('news').innerHTML;
-        const template = Handlebars.compile(templateSource);
-        document.getElementById('news').innerHTML = template({
-            news: json.articles
         });
-    });
+}
 
-//var keyword;
+document.addEventListener('DOMContentLoaded', () => {
+    loadNews('bit');
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('keyword').addEventListener('change', () => {
-        console.log("!!!!!11");
+    document.getElementById('buscar').addEventListener('click', () => {
+        let key = document.getElementById("keyword") as HTMLInputElement;
+        loadNews(key.value);
     });
 });
